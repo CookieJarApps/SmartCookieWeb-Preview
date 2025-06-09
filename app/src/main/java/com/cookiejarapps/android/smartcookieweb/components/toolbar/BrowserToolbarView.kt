5cfea8fcf293b12f9ddeb1f8305d970dae4e520f
tGstep <38408500+tGstep@users.noreply.github.com>
2025-06-09 03:36:42 +0200

    Remove unneeded import

--- app/src/main/java/com/cookiejarapps/android/smartcookieweb/components/toolbar/BrowserToolbarView.kt
+++ app/src/main/java/com/cookiejarapps/android/smartcookieweb/components/toolbar/BrowserToolbarView.kt
@@ -4,7 +4,6 @@ import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.annotation.VisibleForTesting
 import androidx.coordinatorlayout.widget.CoordinatorLayout
